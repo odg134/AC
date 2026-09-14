7deab3c4222599c8860f0829c05941929022ba1f
rs <180887493+odg134@users.noreply.github.com>
2026-09-14 19:52:20 +0100

    add shared IOCTL defs

--- Driver/Core/Dispatch/Handlers/IOCTL/IOCTL.h
+++ Driver/Core/Dispatch/Handlers/IOCTL/IOCTL.h
@@ -1,41 +1,8 @@
 #pragma once
-#include <ntddk.h>
-
-// Define the main IOCTL codes...
-//
-#define IOCTL_HEARTBEAT CTL_CODE( FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS )
-#define IOCTL_PROTECT_PROCESS CTL_CODE( FILE_DEVICE_UNKNOWN, 0x801, METHOD_BUFFERED, FILE_ANY_ACCESS )
-#define IOCTL_UNPROTECT_PROCESS CTL_CODE( FILE_DEVICE_UNKNOWN, 0x802, METHOD_BUFFERED, FILE_ANY_ACCESS )
-#define IOCTL_QUERY_PROTECTION CTL_CODE( FILE_DEVICE_UNKNOWN, 0x803, METHOD_BUFFERED, FILE_ANY_ACCESS )
+#include <IOCTL/IOCTL.h>
 
 namespace IOCTL
 {
-    struct HeartbeatResponse
-    {
-        ULONG Version;
-        BOOLEAN Protected;
-    };
-
-    struct ProtectRequest
-    {
-        ULONG Pid;
-    };
-
-    struct UnprotectRequest
-    {
-        ULONG Pid;
-    };
-
-    struct QueryRequest
-    {
-        ULONG Pid;
-    };
-
-    struct QueryResponse
-    {
-        BOOLEAN Protected;
-    };
-
     NTSTATUS Heartbeat( PIRP Irp, PIO_STACK_LOCATION Stack );
     NTSTATUS ProtectProcess( PIRP Irp, PIO_STACK_LOCATION Stack );
     NTSTATUS UnprotectProcess( PIRP Irp, PIO_STACK_LOCATION Stack );
---
+++ Shared/IOCTL/AC_IOCTL.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifdef _KERNEL_MODE
+#  include <ntddk.h>
+#else
+#  include <windows.h>
+#  include <winioctl.h>
+#endif
+
+#define AC_DEVICE_NAME  L"\\Device\\AC_Driver"
+#define AC_SYMLINK_NAME L"\\DosDevices\\AC_Driver"
+
+#define IOCTL_HEARTBEAT         CTL_CODE( FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS )
+#define IOCTL_PROTECT_PROCESS   CTL_CODE( FILE_DEVICE_UNKNOWN, 0x801, METHOD_BUFFERED, FILE_ANY_ACCESS )
+#define IOCTL_UNPROTECT_PROCESS CTL_CODE( FILE_DEVICE_UNKNOWN, 0x802, METHOD_BUFFERED, FILE_ANY_ACCESS )
+#define IOCTL_QUERY_PROTECTION  CTL_CODE( FILE_DEVICE_UNKNOWN, 0x803, METHOD_BUFFERED, FILE_ANY_ACCESS )
+
+namespace IOCTL
+{
+    struct HeartbeatResponse  { ULONG Version; BOOLEAN Protected; };
+    struct ProtectRequest     { ULONG Pid; };
+    struct UnprotectRequest   { ULONG Pid; };
+    struct QueryRequest       { ULONG Pid; };
+    struct QueryResponse      { BOOLEAN Protected; };
+}
